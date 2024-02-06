970c39fa943f9a3f1fbec6c8b930ac8133688c1c
BuildTools <unconfigured@null.spigotmc.org>
2024-02-06 11:04:35 +0800

    ?

--- src/dComplex.c
+++ src/dComplex.c
@@ -114,6 +114,7 @@ dEuler complex2Euler(dComplex s)
     case Origin:
         return (dEuler){0, 0};
     }
+    return (dEuler){0, 0}; // arm toolchain complains without this handle
 }
 dComplex euler2Complex(dEuler s)
 {
